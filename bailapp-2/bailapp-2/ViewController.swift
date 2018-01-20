3a19b9341bd73eb510fd504c9f426930264cda1a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-01-19 23:32:21 -0500

    change fields

---
+++ bailapp-2/bailapp-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bailapp-2
+//
+//  Created by Ricardo Herrera Petit on 1/19/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
